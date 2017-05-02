932c5942763f3b5c1fbb1b7dd4361371dc4a05c4
jenkrisu <jenni.unkuri@gmail.com>
2017-05-02 21:03:42 +0300

    Add changes

--- app/src/main/java/net/bookbuddy/MainActivity.java
+++ app/src/main/java/net/bookbuddy/MainActivity.java
@@ -1,10 +1,10 @@
 package net.bookbuddy;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-
 /**
  * Shows welcome message and opens the application.
  */
@@ -28,12 +28,38 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    /**
+     * Starts Search Activity.
+     *
+     * @param view
+     */
     public void skip(View view) {
         startActivity(new Intent(this, SearchActivity.class));
     }
 
     public void login(View view) {
+        /*final OAuth10aService service = new ServiceBuilder()
+                .apiKey(BuildConfig.GOOD_READS_API_KEY)
+                .apiSecret(BuildConfig.GOOD_READS_API_SECRET)
+                .build(GoodReadsApi.instance());
 
+        Token requestToken = null;
+
+        try {
+            requestToken = service.getRequestToken();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+        System.out.println(requestToken.toString());*/
+
+        /*Uri uri = Uri.parse("http://goodreads.com/oauth/authorize")
+                .buildUpon()
+                .appendQueryParameter("key", BuildConfig.GOOD_READS_API_KEY)
+                .appendQueryParameter("mobile", "1")
+                .build();
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(intent);*/
     }
 
 }
