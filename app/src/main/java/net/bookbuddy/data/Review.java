e659e9ac8513be38c77e4688bc438cf951597a01
jenkrisu <jenni.unkuri@gmail.com>
2017-05-09 21:26:28 +0300

    Add Review data and parser classes

---
+++ app/src/main/java/net/bookbuddy/data/Review.java
@@ -0,0 +1,8 @@
+package net.bookbuddy.data;
+
+/**
+ * Created by Jenni on 9.5.2017.
+ */
+
+public class Review {
+}
---
+++ app/src/main/java/net/bookbuddy/utilities/ReviewResultParser.java
@@ -0,0 +1,8 @@
+package net.bookbuddy.utilities;
+
+/**
+ * Created by Jenni on 9.5.2017.
+ */
+
+public class ReviewResultParser {
+}
