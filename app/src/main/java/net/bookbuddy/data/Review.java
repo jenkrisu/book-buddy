a8457870b740f17beaaf3e775a6f820e4a93975f
jenkrisu <jenni.unkuri@gmail.com>
2017-05-10 05:50:11 +0300

    Comment class

--- app/src/main/java/net/bookbuddy/data/Review.java
+++ app/src/main/java/net/bookbuddy/data/Review.java
@@ -8,27 +8,70 @@ import java.util.List;
 
 public class Review {
 
+    /**
+     * Id of review.
+     */
     private String id;
 
+    /**
+     * Book.
+     */
     private Book book;
 
+    /**
+     * Rating of book connected to this review.
+     */
     private String rating;
 
+    /**
+     * Date when started reading book.
+     */
     private String startedAt;
 
+    /**
+     * Date when finished reading book.
+     */
     private String readAt;
 
+    /**
+     * Date when book added to shelves.
+     */
     private String dateAdded;
 
+    /**
+     * Date when book updated.
+     */
     private String dateUpdated;
 
+    /**
+     * Book review.
+     */
     private String body;
 
+    /**
+     * Shelves book connected to this review is on.
+     */
     private List<Shelf> shelves;
 
+    /**
+     * Default constructor.
+     */
     public Review() {
     }
 
+    /**
+     * Creates review.
+     *
+     * @param id          String book id
+     * @param book        Book book
+     * @param rating      String rating of book
+     * @param startedAt   String date when started reading book
+     * @param readAt      String date when finished reading book
+     * @param dateAdded   String date when book added
+     * @param dateUpdated String date when book updated
+     * @param body        String review body
+     * @param shelves     List shelves
+     */
     public Review(String id, Book book, String rating, String startedAt, String readAt,
                   String dateAdded, String dateUpdated, String body, List<Shelf> shelves) {
         this.id = id;
@@ -42,37 +85,79 @@ public class Review {
         this.shelves = shelves;
     }
 
+    /**
+     * Gets id.
+     *
+     * @return String id
+     */
     public String getId() {
         return id;
     }
 
+    /**
+     * Gets book.
+     *
+     * @return Book book
+     */
     public Book getBook() {
         return book;
     }
 
+    /**
+     * Gets rating.
+     *
+     * @return String rating
+     */
     public String getRating() {
         return rating;
     }
 
+    /**
+     * Gets startedAt.
+     *
+     * @return String starting date
+     */
     public String getStartedAt() {
         return startedAt;
     }
 
+    /**
+     * Gets readAt.
+     *
+     * @return String reading date
+     */
     public String getReadAt() {
         return readAt;
     }
 
+    /**
+     * Gets dateAdded.
+     *
+     * @return String date added
+     */
     public String getDateAdded() {
         return dateAdded;
     }
 
+    /**
+     * Gets dateUpdated.
+     *
+     * @return String date updated
+     */
     public String getDateUpdated() {
         return dateUpdated;
     }
 
+    /**
+     * Gets review text.
+     *
+     * @return String review text
+     */
     public String getBody() {
         return body;
     }
 
-    public List<Shelf> getShelves() { return shelves; }
+    public List<Shelf> getShelves() {
+        return shelves;
+    }
 }
